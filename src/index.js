398cfe63968e8ce60a73aa038b6622c9654586e6
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-16 22:05:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  let events = []
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let newTask = e.target.children[1].value
+    if (newTask != "") {
+      events.push(newTask)
+    }
+    console.log(events)
+    updateTasks()
+  })
+
+  function updateTasks() {
+    let list = document.getElementById('tasks')
+    for (var i = 0; i < events.length; i++) {
+      let item = document.createElement('li')
+      item.innerText = events[i]
+      list.appendChild(item)
+    }
+  }
 });
